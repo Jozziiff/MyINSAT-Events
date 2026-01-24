eea1ab59670ceb1c4bdc040ce1acd68feba1d326
Jozziiff (MonsterV2) <youssef.hamdani2000@gmail.com>
2026-01-24 14:28:08 +0100

    feat: add profile minimalistic design with events and clubs sections

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home';
 import { EventsComponent } from './pages/events/events';
 import { ClubsComponent } from './pages/clubs/clubs';
+import { ProfilePage } from './pages/profile/profile-page/profile-page';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent }, // Default route
   { path: 'events', component: EventsComponent },
-  { path: 'clubs', component: ClubsComponent }
+  { path: 'clubs', component: ClubsComponent },
+  { path: 'profile', component: ProfilePage}
   // We will add login and profile here later
 ];
